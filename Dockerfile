f8b5c3a7edad73a19e5e38fd534ea455af0f7819
PrasadVdm <vdmprasad2@gmail.com>
2024-11-09 16:03:10 +0000

    test-2 branch

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python:3.12-alpine
-RUN pip install flask
-WORKDIR /myapp
-COPY main.py /myapp/main.py
-CMD ["python3", "/myapp/main.py"]
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello from kick buttowski v1.0"
+        return "Hello from cicd pipeline 2"
 
 if __name__ == "__main__":
         app.run(host='0.0.0.0', port=8080)
